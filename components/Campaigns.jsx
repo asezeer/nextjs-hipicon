b316e6f526a72847e734e4ab70b4842c90475be5
AYDIN SEZER <a.sezeer@gmail.com>
2022-11-10 14:57:14 +0300

    Update Campaigns.jsx

--- components/Campaigns.jsx
+++ components/Campaigns.jsx
@@ -2,51 +2,12 @@ import Image from "next/image";
 import Title from "./ui/Title";
 import { MdShoppingCart } from "react-icons/md";
 
-const CampaignItem = () => {
+const CampaignItem = ({ as }) => {
   return (
     <div className="bg-secondary flex-1 rounded-md py-5 px-[15px] flex items-center gap-x-4">
       <div className="relative md:w-44 md:h-44 w-36 h-36 after:content-['']   border-[5px] border-primary rounded-full overflow-hidden">
         <Image
-          src="/images/carousel1.png"
-          alt=""
-          layout="fill"
-          className="hover:scale-105 transition-all"
-          objectFit="cover"
-        />
-      </div>
-      <div className="text-white">
-        <Title addClass="text-2xl">Tasty Thursdays</Title>
-        <div className="font-dancing my-1">
-          <span className="text-[40px]">20%</span>
-          <span className="text-sm inline-block ml-1">Off</span>
-        </div>
-        <button className="btn-primary flex items-center gap-x-2">
-          Order Now <MdShoppingCart size={20} />
-        </button>
-      </div>
-
-      <div className="relative md:w-44 md:h-44 w-36 h-36 after:content-['']   border-[5px] border-primary rounded-full overflow-hidden">
-        <Image
-          src="/images/carousel4.png"
-          alt=""
-          layout="fill"
-          className="hover:scale-105 transition-all"
-          objectFit="cover"
-        />
-      </div>
-      <div className="text-white">
-        <Title addClass="text-2xl">Tasty Thursdays</Title>
-        <div className="font-dancing my-1">
-          <span className="text-[40px]">20%</span>
-          <span className="text-sm inline-block ml-1">Off</span>
-        </div>
-        <button className="btn-primary flex items-center gap-x-2">
-          Order Now <MdShoppingCart size={20} />
-        </button>
-      </div>
-      <div className="relative md:w-44 md:h-44 w-36 h-36 after:content-['']   border-[5px] border-primary rounded-full overflow-hidden">
-        <Image
-          src="/images/carousel2.png"
+          src={as}
           alt=""
           layout="fill"
           className="hover:scale-105 transition-all"
@@ -67,12 +28,15 @@ const CampaignItem = () => {
   );
 };
 
-const Campaigns = () => {
+const Campaigns = (as) => {
   return (
     <div className="flex justify-between container mx-auto py-20 gap-6 flex-wrap">
-      <CampaignItem />
+      <CampaignItem as="/images/carousel3.png" />
+      <CampaignItem as="/images/carousel1.png" />
+      <CampaignItem as="/images/carousel4.png" />
     </div>
   );
 };
 
 export default Campaigns;
+
